37f7d10ab77e29c2ff4ef0721241ef3434c1f40c
Davidru <thedavidro@gmail.com>
2017-05-08 00:37:10 +0200

    ej6 [not working] + Camera class
    ejercicio 6 utilizando la clase Object (.cpp y .h) junto a la clase
    Camera + nuevos shaders para la iluminacon

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,9 @@
 #include <iostream>
 #include "Model.h" //model tiene el Mesh.h y Mesh.h tiene el Shader.h
 #include "Object.h"
+#include "Camera.h" // matrix_transform + glm.hpp
 #include <SOIL.h>
 
-#include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
 #include <gtc/type_ptr.hpp>
 
 
@@ -22,24 +21,15 @@ GLfloat mixValue = 0.6f;
 float rotationX = 0.0f;
 float rotationY = 0.0f;
 
-enum KeyPressed {
-	keyW, keyA, keyS, keyD
-};
 
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-GLfloat yaw = -90.0f;
-GLfloat pitch = 0.0f;
+glm::vec3 vectorUp = glm::vec3(0.0f, 1.0f, 0.0f);
 GLfloat lastX = WIDTH / 2.0;
 GLfloat lastY = HEIGHT / 2.0;
-GLfloat fov = 45.0f;
-bool firstTime = true;
-
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 
 int choosenObj = 1;
+bool firstMouse = true;
 
 glm::vec3 cube1pos = { 0,0,0 };
 GLfloat cube1offsetX;
@@ -50,12 +40,14 @@ glm::vec3 cube2pos = { 0,0,0 };
 GLfloat cube2offsetX;
 GLfloat cube2offsetY;
 GLfloat cube2offsetZ;
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+Camera myCam(glm::vec3(0.0f, 0.0f, 0.0f), vectorUp, -90.0f, 45.0f);
 
 //funciones
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void do_movement(KeyPressed key);
 
 int main() {
 	//initGLFW
@@ -97,6 +89,7 @@ int main() {
 
 	//GLuint programID = LoadShaders("./src/SimpleVertexShader.vertexshader", "./src/SimpleFragmentShader.fragmentshader");
 	Shader myShader("./src/SimpleVertexShader.vertexshader", "./src/SimpleFragmentShader.fragmentshader");
+	Shader lightingShader("./src/LightVertexShader.vertexshader", "./src/LightFragmentShader.fragmenshader");
 	GLuint shaderVar = glGetUniformLocation(myShader.Program, "offset");
 	// Definir el buffer de vertices
 
@@ -170,9 +163,6 @@ int main() {
 		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
-	Shader lightingShader("path/to/shaders/lighting.vs", "path/to/shaders/lighting.frag");
-	Shader lampShader("path/to/shaders/lamp.vs", "path/to/shaders/lamp.frag");
-
 	GLuint VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
@@ -203,7 +193,8 @@ int main() {
 	glm::vec3 vector(3);
 	glm::mat2 matriz;
 
-	Object cubo1(glm::vec3 = { 1,1,1 }, glm::vec3 = { 0.3,0.3,0.3 }, cube1pos, 0);
+	Object cubo1(glm::vec3(1.f, 1.f, 1.f), glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 0.f, 0.f), Object::cube);
+	Object cubo2(glm::vec3(0.1f, 0.1f, 0.1f), glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 0.f, 0.f), Object::cube);
 
 	//Object cube1(glm::vec3(1.f, 1.f, 1.f), (0.3f, 0.3f, 0.3f), (0.0f, 0.0f, 0.0f), 0);
 
@@ -216,54 +207,55 @@ int main() {
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		glClearColor(0.f, 0.f, 0.f, 1.f);
+		glfwPollEvents();
 
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glfwPollEvents(); //llama a do_movement
+		lightingShader.Use();
+		GLint objectColorLoc = glGetUniformLocation(lightingShader.Program, "objectColor");
+		GLint lightColorLoc = glGetUniformLocation(lightingShader.Program, "lightColor");
+		GLint lightPosLoc = glGetUniformLocation(lightingShader.Program, "lightPos");
+		GLint viewPosLoc = glGetUniformLocation(lightingShader.Program, "viewPos");
+		glUniform3f(objectColorLoc, 1.0f, 0.5f, 0.31f);
+		glUniform3f(lightColorLoc, 1.0f, 1.0f, 1.0f);
+		glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
+		glUniform3f(viewPosLoc, myCam.camPos().x, myCam.camPos().y, myCam.camPos().z);
 
-		myShader.Use();
 
+
+		// Create camera transformations
 		glm::mat4 view;
-		glm::mat4 projection;
-
-		glm::vec3 movementVector = glm::vec3(cube1pos.x + cube1offsetX, cube1offsetY + cube1offsetY, cube1pos.z + cube1offsetZ);
-		glm::vec3 rotationVector = glm::vec3(rotationX, rotationY, 0.0f);
-
-
-		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-		projection = glm::perspective(fov, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
-
-		cube1.Move(movementVector);
-		cube1.Rotate(rotationVector);
-		model = cubeA.GetModelMatrix();
-
-		GLint modelLoc = glGetUniformLocation(myShader.Program, "model");
-		GLint viewLoc = glGetUniformLocation(myShader.Program, "view");
-		GLint projLoc = glGetUniformLocation(myShader.Program, "projection");
-
+		view = myCam.LookAt();
+		glm::mat4 projection = glm::perspective(myCam.GetFOV(), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+		// Get the uniform locations
+		GLint modelLoc = glGetUniformLocation(lightingShader.Program, "model");
+		GLint viewLoc = glGetUniformLocation(lightingShader.Program, "view");
+		GLint projLoc = glGetUniformLocation(lightingShader.Program, "projection");
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		glm::mat4 model;
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-		for (GLuint i = 0; i < 10; i++)
-		{
-			// Calculate the model matrix for each object and pass it to shader before drawing
-			glm::mat4 model;
-			model = glm::translate(model, CubesPositionBuffer[i]);
-			GLfloat angle = 20.0f * i;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			model = glm::rotate(model, glm::radians(rotationX), glm::vec3(1.0f, 0.0f, 0.0f));
-			model = glm::rotate(model, glm::radians(rotationY), glm::vec3(0.0f, 1.0f, 0.0f));
-			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		cubo1.Draw();
+		glBindVertexArray(0);
 
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+		// Also draw the lamp object, again binding the appropriate shader
+		myShader.Use();
+		// Get location objects for the matrices on the lamp shader (these could be different on a different shader)
+		modelLoc = glGetUniformLocation(myShader.Program, "model");
+		viewLoc = glGetUniformLocation(myShader.Program, "view");
+		projLoc = glGetUniformLocation(myShader.Program, "projection");
+		// Set matrices
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		model = glm::mat4();
+		model = glm::translate(model, lightPos);
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		cubo2.Draw();
+		glBindVertexArray(0);
 
-		//intercambia el framebuffer
-
-		
-
-
+		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
 
@@ -280,98 +272,27 @@ int main() {
 	exit(EXIT_SUCCESS);
 }
 
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-	if (firstTime)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstTime = false;
-	}
-
-	GLfloat xoffset = xpos - lastX;
-	GLfloat yoffset = lastY - ypos; // Reversed since y-coordinates go from bottom to left
-	lastX = xpos;
-	lastY = ypos;
-
-	GLfloat sensitivity = 0.05;	// Change this value to your liking
-	xoffset *= sensitivity;
-	yoffset *= sensitivity;
-
-	yaw += xoffset;
-	pitch += yoffset;
-
-	// Make sure that when pitch is out of bounds, screen doesn't get flipped
-	if (pitch > 89.0f)
-		pitch = 89.0f;
-	if (pitch < -89.0f)
-		pitch = -89.0f;
-
-	glm::vec3 front;
-	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-	front.y = sin(glm::radians(pitch));
-	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-	cameraFront = glm::normalize(front);
-}
-
-void do_movement(KeyPressed key) {
-	GLfloat cameraSpeed = 5.0f * deltaTime;
-	switch (key) {
-	case keyW:
-		cameraPos += cameraSpeed * cameraFront;
-		break;
-
-	case keyA:
-		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-		break;
-
-	case keyS:
-		cameraPos -= cameraSpeed * cameraFront;
-		break;
-
-	case keyD:
-		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-		break;
-	}
-}
-
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-	if (fov >= 1.0f && fov <= 45.0f)
-		fov -= yoffset;
-	if (fov <= 1.0f)
-		fov = 1.0f;
-	if (fov >= 45.0f)
-		fov = 45.0f;
-}
-
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 
 	if (key == GLFW_KEY_W && (action == GLFW_REPEAT || action == GLFW_PRESS))
-		do_movement(keyW);
+		myCam.DoMovement(window, Camera::keyW, deltaTime);
 
 	if (key == GLFW_KEY_A && (action == GLFW_REPEAT || action == GLFW_PRESS))
-		do_movement(keyA);
+		myCam.DoMovement(window, Camera::keyA, deltaTime);
 
 	if (key == GLFW_KEY_S && (action == GLFW_REPEAT || action == GLFW_PRESS))
-		do_movement(keyS);
+		myCam.DoMovement(window, Camera::keyS, deltaTime);
 
 	if (key == GLFW_KEY_D && (action == GLFW_REPEAT || action == GLFW_PRESS))
-		do_movement(keyD);
+		myCam.DoMovement(window, Camera::keyD, deltaTime);
 
 	//EJERCICIO 2
 	if (key == GLFW_KEY_4 && action == GLFW_PRESS) {
-		/*mixValue += 0.1f;
-		if (mixValue >= 1.0f)
-		mixValue = 1.0f;*/
 		mixValue = 0;
 	}
 	if (key == GLFW_KEY_5 && action == GLFW_PRESS) {
-		/*mixValue -= 0.1f;
-		if (mixValue <= 0.0f)
-		mixValue = 0.0f;*/
 		mixValue = 1;
 	}
 
@@ -388,15 +309,27 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	if (key == GLFW_KEY_LEFT && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
 		rotationY += 0.5f;
 	}
+}
 
-	// Swap de objetos
-	if (key == GLFW_KEY_1 && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
-		choosenObj = 1;
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
 	}
-	if (key == GLFW_KEY_2 && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
-		choosenObj = 2;
-	}
-	if (key == GLFW_KEY_3 && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
-		choosenObj = 3;
-	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;  // Reversed since y-coordinates go from bottom to left
+
+	lastX = xpos;
+	lastY = ypos;
+
+	myCam.MouseMove(window, xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	myCam.MouseScroll(window, xoffset, yoffset);
 }
